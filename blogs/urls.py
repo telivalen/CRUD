893eddf5df0026cbb5a5f8be9bfc48b958ae0fd2
lastname <telivalen@icloud.com>
2024-03-16 16:52:10 -0400

    create

---
+++ blogs/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import BlogListView, BlogCreateView
+
+urlpatterns = [
+    path("", BlogListView.as_view(), name="home"),
+    path("post/new/", BlogCreateView.as_view(), name="post_new"),
+]
