bf386d1843ad1240ad598767a1aab0f916a26d62
lastname <telivalen@icloud.com>
2024-03-23 16:42:20 -0400

    update add

--- blogs/views.py
+++ blogs/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Publication
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 # Create your views here.
 
 class BlogListView(ListView):
@@ -9,10 +9,16 @@ class BlogListView(ListView):
     template_name = 'home.html'
 
 class BlogCreateView(CreateView):
-        model = Publication
-        template_name = 'post_new.html'
-        fields = ['title', 'body', 'author']
+    model = Publication
+    template_name = 'post_new.html'
+    fields = ['title', 'body', 'author']
 
 class BlogDetailView(DetailView):
-      model = Publication
-      template_name = 'post_detail.html'
+    model = Publication
+    template_name = 'post_detail.html'
+
+class BlogUpdateView(UpdateView):
+    model = Publication
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']
+
