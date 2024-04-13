d6d9ae2660ed9891ff16b14590122b4438326044
lastname <telivalen@icloud.com>
2024-04-13 15:19:40 -0400

    signup added

--- django_project/urls.py
+++ django_project/urls.py
@@ -21,5 +21,6 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blogs.urls')),
+    path('account/', include('account.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
